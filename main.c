95dc04ae022a81e887551e1618915dc78ff03716
Brian <bcj26@case.edu>
2018-04-16 15:15:08 -0400

    better commenting and minor refactor

--- main.c
+++ main.c
@@ -1,21 +1,34 @@
+/*H**********************************************************************
+* FILENAME :        main.c
+*
+* DESCRIPTION :
+*       Uses multiple GPUs to compute the largest eigenvalue/eigenvector of a matrix A. 
+*	This eigenpair is computed using the power iteration algorithm. 
+* 
+* AUTHOR :    Brian Johnosn        START DATE :    16 April, 2018
+*
+*H*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 /*
- 	This code to load a .csv into a 2d matrix is 100% stolen from 
-	https://stackoverflow.com/questions/20013693/read-csv-file-to-a-2d-array-on-c
-	I make 0 claim to this code. 
+*	Returns matrix stored in filename (assumed .csv) as a matrix of floats
+*
+*	@input filename (should be .csv)
+*	@output matrix of floats stored in filename
+*
+* 	This code to load a .csv into a 2d matrix is 100% stolen from 
+*	https://stackoverflow.com/questions/20013693/read-csv-file-to-a-2d-array-on-c
+*	I make 0 claim to this code. 
+*
 */
-
-
-
-int main()
+float get_matrix (char *filename)
 {
    char buffer[1024*1024] ;
    char *record,*line;
    int i=0,j=0;
-   int mat[1000][1000];
-   FILE *fstream = fopen("test_mat_1.csv","r");
+   float mat[1000][1000];
+   FILE *fstream = fopen(filename,"r");
    if(fstream == NULL)
    {
       printf("\n file opening failed ");
@@ -27,10 +40,22 @@ int main()
      while(record != NULL)
      {
      printf("%s",record) ;    //here you can put the record into the array as per your requirement.
-     mat[i][j++] = atoi(record) ;
+     mat[i][j++] = atof(record) ;
      record = strtok(NULL,";");
      }
      ++i ;
    }
    return 0 ;
  }
+
+
+/*
+*	Main function for GPU power iteration code
+*	@input None TODO: Should take filename as CMD arg
+*	@output Largest eigenvalue of matrix 
+*/
+int main(){
+	get_matrix("test_mat_1.csv");
+	return 0;
+	
+}
